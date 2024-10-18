7e7469a14333d7a7aa905a54290ba2aed003e184
Carambole <131371657+carambole25@users.noreply.github.com>
2024-10-18 11:37:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include "manager.hpp"
 
+using namespace std;
+
 void unitTest(){
     Manager manager;
     manager.decrypt("roch");
@@ -11,52 +13,52 @@ void unitTest(){
 
 void ui() {
     Manager manager;
-    std::string command;
-    std::string key;
-    std::cout << "Entrez la clé de chiffrement : ";
-    std::getline(std::cin, key);
+    string command;
+    string key;
+    cout << "Entrez la clé de chiffrement : ";
+    getline(cin, key);
 
     manager.decrypt(key);
 
     while (true) {
-        std::cout << "\n-----------------------------------\n|     Que voulez vous faire ?     |" << std::endl;
-        std::cout
+        cout << "\n-----------------------------------\n|     Que voulez vous faire ?     |" << endl;
+        cout
                 << "-----------------------------------\n|   a : ajouter                   |\n|   l : afficher un objet         |\n|   r : enlever un objet          |\n|   p : afficher tous les objets  |\n|   v : tester un mdp             |\n|   g : générer un mdp            |\n|   q : quitter                   |\n-----------------------------------"
-                << std::endl;
-        std::cout << "> ";
-        std::getline(std::cin, command);
+                << endl;
+        cout << "> ";
+        getline(cin, command);
 
         if (command == "a") {
-            std::string platform;
-            std::string id;
-            std::string password;
-            std::string twofaString;
-            std::cout << "platform : ";
-            std::getline(std::cin, platform);
-            std::cout << "id : ";
-            std::getline(std::cin, id);
-            std::cout << "password : ";
-            std::getline(std::cin, password);
+            string platform;
+            string id;
+            string password;
+            string twofaString;
+            cout << "platform : ";
+            getline(cin, platform);
+            cout << "id : ";
+            getline(cin, id);
+            cout << "password : ";
+            getline(cin, password);
             if (password == "") {
                 password = manager.passwordGenerator();
-                std::cout << "Mot de passe généré : " << password << std::endl;
+                cout << "Mot de passe généré : " << password << endl;
             }
-            std::cout << "Voulez vous ajouter une 2fa ? (y/n) : ";
-            std::getline(std::cin, twofaString);
+            cout << "Voulez vous ajouter une 2fa ? (y/n) : ";
+            getline(cin, twofaString);
             if (twofaString == "y") {
                 manager.add(platform, id, password, true);
             } else {
                 manager.add(platform, id, password, false);
             }
         } else if (command == "l") {
-            std::string platform;
-            std::cout << "\nplatform : ";
-            std::getline(std::cin, platform);
+            string platform;
+            cout << "\nplatform : ";
+            getline(cin, platform);
             manager.get(platform);
         } else if (command == "r") {
-            std::string platform;
-            std::cout << "platform : ";
-            std::getline(std::cin, platform);
+            string platform;
+            cout << "platform : ";
+            getline(cin, platform);
             manager.remove(platform);
         } else if (command == "p") {
             manager.print();
@@ -64,15 +66,15 @@ void ui() {
             manager.encrypt(key);
             break;
         } else if (command == "v") {
-            std::string password;
-            std::cout << "password : ";
-            std::getline(std::cin, password);
+            string password;
+            cout << "password : ";
+            getline(cin, password);
             manager.checkSafety(password);
         } else if (command == "g") {
-            std::cout << "Mot de passe généré : " << manager.passwordGenerator() << std::endl;
+            cout << "Mot de passe généré : " << manager.passwordGenerator() << endl;
         }
         else {
-            std::cout << "Commande inconnue" << std::endl;
+            cout << "Commande inconnue" << endl;
         }
     }
 }
